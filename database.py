8299904796ba96485bca524a2362c65e182f3a60
Vishesh Agarwal <visheshagarwal1123@gmail.com>
2026-06-05 18:19:53 +0530

    Create Note database model

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 DATABASE_URL = "sqlite:///./notes.db"
 
---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+class NoteDB(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
