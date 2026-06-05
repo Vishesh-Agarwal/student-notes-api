eec06ddebd419c94d7831361aaf9b9cbb57348ee
Vishesh Agarwal <visheshagarwal1123@gmail.com>
2026-06-05 20:37:24 +0530

    Add validation constraints for notes

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Note(BaseModel):
-    title: str
-    content: str
+    title: str = Field(min_length=3, max_length=50)
+    content: str = Field(min_length=5, max_length=200)
